4ebfb72c7baef8130e1ae589c6a5e5e87909cef4
desmond-chan <31633021+desmond-chan@users.noreply.github.com>
2025-12-25 15:53:26 -0500

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-name = sys.argv[1]          # first argument after the script name
+arg = sys.argv[1]          # first argument after the script name
 if "=" in arg:
     key, value = arg.split("=", 1)
     name = value
