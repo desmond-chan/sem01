f7e63acec1f8aa4d9c26bb96ad9e2b7b347d42dd
desmond-chan <31633021+desmond-chan@users.noreply.github.com>
2025-12-25 15:52:21 -0500

    Correct argument parsing in hello2.py
    Fix argument handling to correctly assign name.

--- hello2.py
+++ hello2.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 import sys
 name = sys.argv[1]          # first argument after the script name
+if "=" in arg:
+    key, value = arg.split("=", 1)
+    name = value
+else:
+    name = arg
+
 print("My name is ", name)
